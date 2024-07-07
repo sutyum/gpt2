2d6f1bd6b62a7218411d75f8240411c09195542a
satyam <satyam@technoculture.io>
2024-07-07 18:34:40 +0000

    starting with ddp

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
-import tiktoken
+import os
+import time
 import math
+import inspect
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import tiktoken
 
 
 debug = False
@@ -285,6 +288,32 @@ class GPT2(nn.Module):
 
         return model
 
+    def configure_optimizer(self, weight_decay, learning_rate, device):
+        # Start with all the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+
+        # create optim groups, Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e, all weight tensors in matmuls + embeddings decay, all biases and layernorms dont
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed paramter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed paramter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+
+        # Create AdamW optimizer and use the fused version if is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8)
+        return optimizer
+
 
 class DataLoaderLite:
     def __init__(self, B, T):
@@ -322,7 +351,24 @@ class DataLoaderLite:
         return x, y
 
 
-import time
+
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 500
+
+def get_lr(it: int) -> float:
+    # 1) linear warmup for warmup iter steps
+    if it <= warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3) in between, use cosine decay down to min learing rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
 
 if __name__ == "__main__":
     # Check if we have a GPU
@@ -332,6 +378,13 @@ if __name__ == "__main__":
     elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
         device = "mps"
     print(f"using device: {device}")
+
+    from torch.distributed import init_process_group, destroy_process_group
+
+    # set up DDP (distributed data parallel)
+    # torchrun command sets the env variables RANK, LOCAL_RANK, and WORLD_SIZE
+    ddp = int(os.environ.get('RANK', -1)) != -1 # is this a ddp run?
+    print(ddp, os.environ.get('RANK'), os.environ.get('LOCAL_RANK'))
 
     # Set the random seed for reproducibility
     torch.manual_seed(1337)
@@ -354,13 +407,16 @@ if __name__ == "__main__":
     # Training
     losses = []
     avg_losses = []
-    epochs = 100
     # moving_window_length = 200
 
-    print(f"Training for {epochs} epochs, {model.config.context_size * 16 * 20} tokens")
-    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    print(f"Training for {max_steps} steps")
 
-    for i in range(epochs):
+    # optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    # optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8) # IMPROVEMENTS: Match hyperperameters to GPT3 paper details
+    # IMPROVEMENTS: refactor optimizer into the model
+    optimizer = model.configure_optimizer(weight_decay=0.1, learning_rate=6e-4, device=device)
+
+    for step in range(max_steps):
         t0 = time.time()
         x, y = train_loader.next_batch()
         x, y = x.to(device), y.to(device)
@@ -374,13 +430,20 @@ if __name__ == "__main__":
         # logits, loss = model(x, y) # Baseline
 
         loss.backward()
+        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # IMPROVEMENTS!: Normalize Gradients using norm of gradients
+
+        # IMPROVEMENTS!: Cosine LR Scheduling
+        # determine and set the learning rate for this iteration
+        lr = get_lr(step)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
         optimizer.step()
         torch.cuda.synchronize() # Wait for GPUs to complete the above queued up tasks
 
         t1 = time.time()
         dt = (t1 - t0) * 1000 # milliseconds
         tps = (train_loader.B * train_loader.T) * (t1 - t0)
-        print(f"step {i}, loss = {loss}, dt = {dt}, tokens/sec = {tps}")
+        print(f"step {step} | loss: {loss.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt = {dt} | tokens/sec: {tps}")
 
         losses.append(loss.item())
 
